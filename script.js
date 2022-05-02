71f819f690f9a305131df660e330549ef6bc251f
Sarkoma-kisa <arti.kuryachiy@gmail.com>
2022-05-02 17:52:04 +0300

    added ui

--- script.js
+++ script.js
@@ -16,42 +16,53 @@ let computer = 0
 function playRound(playerSelected, computerSelected) {
     if (playerSelected == 'rock' && computerSelected == 'paper') {
         computer += 1
-        return 'You lose! Paper beats Rock'
+        return resultInfo.textContent = 'You lose! Paper beats Rock'
     } else if (playerSelected == 'rock' && computerSelected == 'scissors') {
         player += 1
-        return 'You win! Rock beats Scissors'
+        return resultInfo.textContent = 'You win! Rock beats Scissors'
     } else if (playerSelected == 'paper' && computerSelected == 'rock'){
         player += 1
-        return 'You win! Paper beats Rock'
+        return  resultInfo.textContent = 'You win! Paper beats Rock'
     } else if (playerSelected == 'paper' && computerSelected == 'scissors'){
         computer += 1
-        return 'You lose! Scissors beats Paper'
+        return  resultInfo.textContent = 'You lose! Scissors beats Paper'
     } else if (playerSelected == 'scissors' && computerSelected == 'paper'){
         player += 1
-        return 'You win! Scissors beats Paper'
+        return  resultInfo.textContent = 'You win! Scissors beats Paper'
     } else if (playerSelected == 'scissors' && computerSelected == 'rock'){
         computer += 1
-       return 'You lose! Rock beats Scissors'
+       return  resultInfo.textContent = 'You lose! Rock beats Scissors'
     } else {
-        return 'Draw'
+        return  resultInfo.textContent = 'Draw'
     }
 }
 
-function game () {
-    for (i = 0; i < 5; i++) {
-        const playerSelected = prompt('Rock, Paper or Scissors?').toLowerCase()
-        const computerSelected = computerPlay()
+const rock = document.getElementById('rock')
+const paper = document.getElementById('paper')
+const scissors = document.getElementById('scissors')
+const resultInfo = document.getElementById('resultInfo')
+const playerScore = document.getElementById('playerScore')
+const computerScore = document.getElementById('computerScore')
+const winner = document.getElementById('winner')
 
-        console.log(playRound(playerSelected, computerSelected)) 
+rock.addEventListener('click', () => handleClick('rock'))
+paper.addEventListener('click', () => handleClick('paper'))
+scissors.addEventListener('click', () => handleClick('scissors'))
+
+function gameOver(player, computer) {
+    if(player == 5) {
+        return winner.textContent = 'You Won'
+    } else if (computer == 5) {
+        return winner.textContent = 'You lost'
     }
-    function winner(player, computer) {
-        if (player > computer) {
-            return 'Player Wins!'
-        } else {
-            return 'Computer Wins!'
-        }
-    }
-    console.log(winner(player, computer))
 }
 
-game()
+function handleClick(playerSelected) {
+    const computerSelected = computerPlay()
+    playRound(playerSelected, computerSelected)
+
+    playerScore.textContent = `Player: ${player}`
+    computerScore.textContent = `Computer: ${computer}`
+    
+    gameOver(player, computer)
+}
